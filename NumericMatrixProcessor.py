1cd7dcbffcb0763a5f2f7ec9311f0df61ede2024
DAG <acostagdiego@gmail.com>
2020-07-11 18:39:27 -0300

    Stage 5 complete

--- NumericMatrixProcessor.py
+++ NumericMatrixProcessor.py
@@ -4,7 +4,7 @@ class Matrix:
     def __init__(self, n=0, m=0, data=None):
         self.row_n = n
         self.col_n = m
-        self.data = [data[m*i:m*(i+1)] for i in range(n)]  # Probably should add self.rows = self.data
+        self.data = [data[m*i:m*(i+1)] for i in range(n)]  # Probably should add self.rows = self.data for clarity.
         self.columns = [data[i::m] for i in range(m)]
 
     def __repr__(self):
@@ -28,6 +28,45 @@ class Matrix:
             print(self.OP_ERROR)
             return Matrix()
 
+    def transpose(self):
+        """Return a transposed version of the Matrix."""
+        return Matrix(self.col_n, self.row_n, [num for col in self.columns for num in col])
+
+    def side_transpose(self):
+        """Return a side diagonal transposed version of the Matrix."""
+        return Matrix(self.col_n, self.row_n, [num for col in self.columns[::-1] for num in col[::-1]])
+
+    def v_transpose(self):
+        """Return a vertically transposed version of the Matrix."""
+        return Matrix(self.row_n, self.col_n, [num for row in self.data for num in row[::-1]])
+
+    def h_transpose(self):
+        """Return a horizontally transposed version of the Matrix."""
+        return Matrix(self.row_n, self.col_n, [num for row in self.data[::-1] for num in row])
+
+    def cofactor(self, i, j):
+        """Return the i, j cofactor of self."""
+        return (-1) ** (i + j) * self.minor(i, j)
+
+    def minor(self, i, j):
+        """Return the i, j minor of self."""
+        return self.submatrix(i, j).det()
+    
+    def det(self):
+        """Return the determinant of self, calculated across the first row. If not possible to calculate, return None."""
+        if self.row_n != self.col_n:
+            print("No determinant defined for non-square matrices.")
+            return None
+        if self.row_n == 1:
+            return self.data[0][0]
+        if self.row_n == 2:
+            return self.data[0][0] * self.data[1][1] - self.data[0][1] * self.data[1][0]
+        return sum(num * self.cofactor(1, j) for j, num in enumerate(self.data[0], 1))
+
+    def submatrix(self, i, j):
+        """Return a Matrix identical to self with row i and column j removed."""
+        return Matrix(self.row_n - 1, self.col_n - 1, [num for n, row in enumerate(self.data, 1) if n != i for m, num in enumerate(row, 1) if m != j])
+
     @staticmethod
     def dot_product(v1, v2):
         """Return the dot product between vectors v1 and v2."""
@@ -37,9 +76,17 @@ def display_menu():
     print("1. Add matrices",
           "2. Multiply matrix by a constant",
           "3. Multiply matrices",
+          "4. Transpose matrix",
+          "5. Calculate a determinant",
           "0. Exit", sep="\n")
 
-def display_result_matrix(m):
+def display_diagonals():
+    print("1. Main diagonal",
+          "2. Side diagonal",
+          "3. Vertical line",
+          "4. Horizontal line", sep="\n")
+
+def display_result(m):
     print("The result is: ", m, sep="\n")
 
 def request_matrix(ordinal="\b"):
@@ -50,7 +97,7 @@ def request_matrix(ordinal="\b"):
     for _ in range(n):
         data += [float(num) if "." in num else int(num) for num in input().split()]
     return Matrix(n, m, data)
-
+    
 while True:
     display_menu()
     choice = int(input("Your choice: "))
@@ -66,5 +113,17 @@ while True:
     elif choice == 3:
         a, b = request_matrix("first"), request_matrix("second")
         result = a * b
-    display_result_matrix(result)
-    print()
+    elif choice == 4:
+        display_diagonals()
+        diagonal_choice = int(input("Your choice: "))
+        if diagonal_choice == 1:
+            result = request_matrix().transpose()
+        elif diagonal_choice == 2:
+            result = request_matrix().side_transpose()
+        elif diagonal_choice == 3:
+            result = request_matrix().v_transpose()
+        elif diagonal_choice == 4:
+            result = request_matrix().h_transpose()
+    elif choice == 5:
+        result = request_matrix().det()
+    display_result(result)
